99dab94d499054d7a1650ff603cd046c5eff7a8f
Aditi191102 <105860777+Aditi191102@users.noreply.github.com>
2024-10-20 21:54:31 +0530

    Navbar created

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+      <Routes>
+          <Route path='/' element={<Home/>}></Route>
+
+          <Route path='/login' element={<Login/>}></Route>
+
+          <Route path='/signup' element={<Signup/>}></Route>
+
+          <Route path='/dashboard' element={<Dashboard/>}></Route>
+      </Routes>
+    </div>);
+}
+
+export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,81 @@
+const Dashboard = () => {
+    return(
+        <div>
+            Welcome to StudyNotion
+        </div>
+    )
+}
+
+export default Dashboard;
+
+/*
+    import Logo from "../assets/Logo.svg";
+import { Link } from "react-router-dom";
+import {toast} from 'react-hot-toast';
+
+function Navbar(props){
+
+    let isLoggedIn = props.isLoggedIn;
+    let setIsLoggedIn = props.setIsLoggedIn;
+
+    function logInHandler(){
+        if(isLoggedIn===true){
+            setIsLoggedIn(false);
+            toast.success("Logged Out Successfully")
+        }
+        
+        else{
+            setIsLoggedIn(true);
+            toast.success("Logged In Successfully")
+        }
+        
+    }
+
+
+    return(
+        <div className=" flex justify-evenly bg-black h-20 items-center">
+                <Link to="/">
+                    <img src={Logo} alt="Logo" loading="lazy" width={160} height={32}></img>
+                </Link> 
+            
+            <div>
+                <ul  className="flex gap-12 text-white">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>    
+                </ul>
+            </div>
+
+            <div className="flex gap-12 text-white">
+            {!isLoggedIn &&
+                <Link to='/login'>
+                    <button onClick={logInHandler}>Login</button>
+                </Link>
+            }
+            {!isLoggedIn &&
+                <Link to='/signup'>
+                    <button>Sign Up</button>
+                </Link>
+            }
+            {isLoggedIn &&
+                <Link to='/'>
+                    <button onClick={logInHandler}>Logout</button>
+                </Link>
+            }
+            {isLoggedIn &&
+                <Link to='/dashboard'>
+                    <button>Dashboard</button>
+                </Link>
+            }
+            </div>
+        </div>
+    )
+}
+export default Navbar;
+*/
